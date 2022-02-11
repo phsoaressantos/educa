9df906e7c8faca2acae690b9f83c7c65247a95ea
Paulo Henrique Santos <paulosantos@id.uff.br>
2022-02-11 17:37:35 -0300

    CLASSE EVENTO, CURSO E COLECAO

---
+++ src/main/java/com/example/educa/repository/ColecaoRepository.java
@@ -0,0 +1,10 @@
+package com.example.educa.repository;
+
+import com.example.educa.modelo.Colecao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+    public interface ColecaoRepository extends JpaRepository<Colecao, Long> {
+
+    }
+
+
